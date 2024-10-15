4ab75df49b0d77c4cac5229ba404edae72a4f9ef
excelformulabot <115473780+excelformulabot@users.noreply.github.com>
2024-10-16 04:44:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,11 @@ headers = {
 bucket_name = os.getenv('AWS_S3_BUCKET')
 
 # S3 client setup (use your credentials or IAM role)
-s3_client = boto3.client('s3')
+# s3_client = boto3.client('s3')
+s3 = boto3.client('s3', 
+                      aws_access_key_id=os.getenv('AWS_ACCESS_KEY') 
+                      aws_secret_access_key=os.getenv('AWS_SECRET_KEY')
+                      region_name=os.getenv('AWS_REGION'))
 
 # Initialize a global counter and a lock for thread safety
 processed_records_counter = 0
